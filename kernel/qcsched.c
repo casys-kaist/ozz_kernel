c3bb1026e9cab35ed5df1bfdd59ff5d96adb4163
Dae R. Jeong <threeearcat@gmail.com>
2021-07-05 07:58:31 +0000

    qcsched: export hook functions' symbols

--- kernel/qcsched.c
+++ kernel/qcsched.c
@@ -1,15 +1,19 @@
+#include <linux/kernel.h>
 #include <linux/qcsched.h>
 
 noinline void __qcsched_hook(void)
 {
 }
+EXPORT_SYMBOL(__qcsched_hook);
 
 void qcsched_hook_entry(void)
 {
 	__qcsched_hook();
 }
+EXPORT_SYMBOL(qcsched_hook_entry);
 
 void qcsched_hook_exit(void)
 {
 	__qcsched_hook();
 }
+EXPORT_SYMBOL(qcsched_hook_exit);
