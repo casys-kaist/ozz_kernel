03303b2a0c5fc6d95c9fabad9b2474e6d0b56b85
Dae R. Jeong <threeearcat@gmail.com>
2021-07-07 08:00:09 +0000

    kernel/relrazzer: add tramopline
    We don't have to keep out-of-tree modules. Let them be in-tree
    modules.

---
+++ kernel/relrazzer/include/hcall_constant.h
@@ -0,0 +1,29 @@
+#ifndef __HCALL_CONSTANT_H
+#define __HCALL_CONSTANT_H
+
+// RAX input value of hcall request
+#define HCALL_RAX_ID 0x1d08aa3e
+// RAX return value indicating a hcall handled successfully
+#define HCALL_SUCCESS 0x2be98adc
+// RAX return value indicating a bad request
+#define HCALL_INVAL 0xb614e7a
+
+// kvm_run->exit_reason
+#define HCALL_EXIT_REASON 0x33f355d
+#define KVM_EXIT_HCALL HCALL_EXIT_REASON
+
+// Commands saved in kvm_run->hypercall.args[0]
+#define HCALL_INSTALL_BP 0xf477909a
+#define HCALL_ACTIVATE_BP 0x40ab903
+#define HCALL_DEACTIVATE_BP 0xf327524f
+#define HCALL_CLEAR_BP 0xba220681
+#define HCALL_VMI_FUNC_ADDR 0x7ca889f0
+
+// Subcommands for HCALL_VMI_FUN_ADDR (saved in kvm_run->hypercall.args[1])
+#define VMI_TRAMPOLINE 0x939aef52
+#define VMI__TRAMPOLINE 0x72c96001
+#define VMI_HOOK 0x30f4b16
+#define VMI_CURRENT_TASK 0xfb40de5
+#define VMI__PER_CPU_OFFSET0 0x4a157131
+
+#endif /* __HCALL_CONSTANT_H */
---
+++ kernel/relrazzer/trampoline/trampoline.c
@@ -0,0 +1,54 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+
+#include "../include/hcall.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Dae R. Jeong");
+MODULE_DESCRIPTION("Trampoline module");
+MODULE_VERSION("0.01");
+
+/* #define _DEBUG */
+
+static noinline void __trampoline(void)
+{
+	// Do here whatever if needed
+#ifdef _DEBUG
+	int cpu;
+	cpu = get_cpu();
+	printk("%d spinning\n", cpu);
+	mdelay(2000);
+	put_cpu();
+#endif
+}
+
+void trampoline(void)
+{
+	// Do nothing here
+loop:
+	__trampoline();
+	goto loop;
+}
+EXPORT_SYMBOL(trampoline);
+
+static int __init trampoline_init(void) {
+	unsigned long trampoline_addr = (unsigned long)trampoline;
+	unsigned long __trampoline_addr = (unsigned long)__trampoline;
+	pr_info("Installing trampoline\n");
+	pr_info("trampoline addr: %lx\n", trampoline_addr);
+	hypercall(HCALL_VMI_FUNC_ADDR, VMI_TRAMPOLINE, trampoline_addr, 0);
+	pr_info("__trampoline addr: %lx\n", __trampoline_addr);
+	hypercall(HCALL_VMI_FUNC_ADDR, VMI_TRAMPOLINE + 1, __trampoline_addr, 0);
+	return 0;
+}
+
+static void __exit trampoline_exit(void) {
+	pr_info("Uninstalling trampoline\n");
+}
+
+module_init(trampoline_init);
+module_exit(trampoline_exit);
