38f646cb062a33a1c01f5668730a44b046ea3aa0
Dae R. Jeong <threeearcat@gmail.com>
2021-08-20 04:55:26 +0000

    kssb/tests/pso_test: disable irq only when requested

--- kernel/kssb/tests/pso_test.c
+++ kernel/kssb/tests/pso_test.c
@@ -16,43 +16,51 @@ static noinline void pso_test_breakpoint(void)
 	nothing = 0;
 }
 
-__attribute__((softstorebuffer)) static void do_writer(bool do_sleep)
+__attribute__((softstorebuffer)) static void do_writer(bool do_sleep,
+						       bool disable_irq)
 {
 	struct shared_t *ptr = (struct shared_t *)&shared;
 	int *iptr = (int *)kmalloc(sizeof(*ptr->ptr), GFP_KERNEL);
-	local_irq_disable();
+	printk(KERN_INFO "%s: do_sleep: %d disable_irq: %d\n", __func__, do_sleep, disable_irq);
+	if (disable_irq)
+		local_irq_disable();
 	ptr->ptr = iptr;
 	ptr->ready = true;
 	pso_test_breakpoint();
 	if (do_sleep)
 		mdelay(3000);
-	local_irq_enable();
+	if (disable_irq)
+		local_irq_enable();
 }
 
-__attribute__((softstorebuffer)) static void do_reader(bool do_sleep)
+__attribute__((softstorebuffer)) static void do_reader(bool do_sleep,
+						       bool disable_irq)
 {
 	int a;
 	struct shared_t *ptr;
+	printk(KERN_INFO "%s: do_sleep: %d disable_irq: %d\n", __func__, do_sleep, disable_irq);
 	if (do_sleep)
 		mdelay(1000);
 	ptr = (struct shared_t *)&shared;
-	local_irq_disable();
+	if (disable_irq)
+		local_irq_disable();
 	if (ptr->ready) {
 		a = *ptr->ptr;
 		printk("%d\n", a);
 	}
-	local_irq_enable();
+	if (disable_irq)
+		local_irq_enable();
 }
 
-SYSCALL_DEFINE1(ssb_pso_writer, bool, do_sleep)
+SYSCALL_DEFINE2(ssb_pso_writer, bool, do_sleep, bool, disable_irq)
 {
-	do_writer(do_sleep);
+	do_writer(do_sleep, disable_irq);
 	return 0;
 }
 
-SYSCALL_DEFINE1(ssb_pso_reader, bool, do_sleep)
+SYSCALL_DEFINE2(ssb_pso_reader, bool, do_sleep, bool, disable_irq)
 {
-	do_reader(do_sleep);
+	do_reader(do_sleep, disable_irq);
 	return 0;
 }
 
