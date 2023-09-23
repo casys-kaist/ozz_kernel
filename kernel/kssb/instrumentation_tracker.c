307d711bbe07fb74eff89984eea262dfb0a59aa2
Dae R. Jeong <threeearcat@gmail.com>
2023-09-23 12:07:32 +0000

    kssb: improve instrumentation tracker
    Previous implementation was to test the idea and it may cause false
    positives. Current implementation does not cause false positives but
    the kernel may die.

--- kernel/kssb/instrumentation_tracker.c
+++ kernel/kssb/instrumentation_tracker.c
@@ -7,16 +7,38 @@
 #define INST_MAP_SIZE 1 << INST_MAP_BIT
 #define inst_addr_hash(addr) hash_long((uint64_t)addr, INST_MAP_BIT)
 
-static bool inst_map[INST_MAP_SIZE];
+#define BUCKET_SIZE 100
+
+static unsigned int inst_map[INST_MAP_SIZE][BUCKET_SIZE];
+
+#define truncate(addr) ((unsigned int)(intptr_t)addr)
 
 void set_instrumented_address(void *addr)
 {
+	unsigned int truncated = truncate(addr);
 	uint32_t idx = inst_addr_hash(addr);
-	inst_map[idx] = true;
+	for (int i = 0; i < BUCKET_SIZE; i++) {
+		if (inst_map[idx][i] == truncated)
+			return;
+		if (inst_map[idx][i] == 0) {
+			inst_map[idx][i] = truncated;
+			return;
+		}
+	}
+	printk("inst_map exhausted!");
+	for (int i = 0; i < BUCKET_SIZE; i++)
+		printk("%x\n", inst_map[idx][i]);
+	BUG();
 }
 
 bool is_instrumented_address(void *addr)
 {
 	uint32_t idx = inst_addr_hash(addr);
-	return inst_map[idx];
+	for (int i = 0; i < BUCKET_SIZE; i++) {
+		if (inst_map[idx][i] == 0)
+			break;
+		if (inst_map[idx][i] == truncate(addr))
+			return true;
+	}
+	return false;
 }
